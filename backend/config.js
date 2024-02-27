1ebc41e1fa9c4f00d957441f501976755ddf08e5
ashagaire <asha.gaire95@gmail.com>
2024-02-27 23:12:42 +0200

    setup MongoDB database

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://ashagaire:TluDDz4PZLCPpdPw@book-store-mern.70g2cax.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN'
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,26 @@
 import express from "express";
-import {PORT} from "./config.js";
-
+import {PORT, mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () =>{
-    console.log(`App is listrning to port: ${PORT}`);
+app.get("/", (request, response)=>{
+    console.log(request);
+    return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
+
+// app.listen(PORT, () =>{
+//     console.log(`App is listrning to port: ${PORT}`);
+// });
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listning to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
